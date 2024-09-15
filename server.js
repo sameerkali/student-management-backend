975eee5544a0b6c220af6c3b6016b2809ca7b4d1
sameerkali <s.faridi007@gmail.com>
2024-09-15 17:37:29 +0530

    add cors

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cors = require('cors'); // Import the CORS middleware
 const schoolRoutes = require('./routes/schoolRoutes');
 const schoolAdminRoutes = require('./routes/schoolAdminRoutes');
 const subjectRoutes = require('./routes/subjectRoutes');
-
-
 
 // Load environment variables
 dotenv.config();
@@ -16,19 +15,19 @@ connectDB();
 // Initialize the Express app
 const app = express();
 
+// Enable CORS for all origins
+app.use(cors());
+
 // Middleware for parsing JSON bodies
 app.use(express.json());
 
 // Routes
 const userRoutes = require('./routes/userRoutes');
-app.use('/api/users', userRoutes);
 
-app.use('/api/schools', schoolRoutes);
-app.use('/api/schooladmin', schoolAdminRoutes);
-app.use('/api/subjects', subjectRoutes);
-
-
-
+app.use('/api/users', userRoutes);       // 3 APIs
+app.use('/api/schools', schoolRoutes);   // 10 APIs
+app.use('/api/subjects', subjectRoutes); // 6 APIs
+app.use('/api/schooladmin', schoolAdminRoutes); // 1 API
 
 // Port setup
 const PORT = process.env.PORT || 6699;
@@ -37,3 +36,52 @@ const PORT = process.env.PORT || 6699;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+
+
+
+
+
+//old
+
+
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const connectDB = require('./config/db');
+// const schoolRoutes = require('./routes/schoolRoutes');
+// const schoolAdminRoutes = require('./routes/schoolAdminRoutes');
+// const subjectRoutes = require('./routes/subjectRoutes');
+
+
+
+// // Load environment variables
+// dotenv.config();
+
+// // Connect to the database
+// connectDB();
+
+// // Initialize the Express app
+// const app = express();
+
+// // Middleware for parsing JSON bodies
+// app.use(express.json());
+
+// // Routes
+// const userRoutes = require('./routes/userRoutes');
+
+// app.use('/api/users', userRoutes); // 3 api
+// app.use('/api/schools', schoolRoutes); //10 api
+// app.use('/api/subjects', subjectRoutes); // 6 api
+// app.use('/api/schooladmin', schoolAdminRoutes); // 1 api
+
+
+
+
+// // Port setup
+// const PORT = process.env.PORT || 6699;
+
+// // Start server
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
